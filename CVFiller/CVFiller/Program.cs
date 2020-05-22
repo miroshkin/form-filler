264d81063e0e8d02cad4696804e3395018691528
miroshkin <anton.miroshkin@gmail.com>
2020-05-23 00:23:38 +0300

    Good DPI font

--- CVFiller/CVFiller/Program.cs
+++ CVFiller/CVFiller/Program.cs
@@ -14,9 +14,15 @@ namespace FormFiller
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CVForm());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
